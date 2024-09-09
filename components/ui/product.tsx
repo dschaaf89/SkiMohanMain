6b0f3c34fd9299bc5e8fbe26e60c6bd1240db8de
danielschaaf <daniel.schaaf@smoothstack.com>
2024-09-08 19:00:01 -0700

    added multiFile upload to instructor form

--- components/ui/product.tsx
+++ components/ui/product.tsx
@@ -65,6 +65,7 @@ const Product: React.FC<ProductProps> = ({ data }) => {
             <button
               onClick={handleOrder}
               className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition duration-300"
+              disabled={true} 
             >
               Order
             </button>
