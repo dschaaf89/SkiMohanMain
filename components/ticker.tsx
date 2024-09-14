7c4354a8a6f15b6b0c28780ea5324d1bd806f6a5
danielschaaf <daniel.schaaf@smoothstack.com>
2024-09-13 18:32:47 -0700

    made edit to the ticker

--- components/ticker.tsx
+++ components/ticker.tsx
@@ -6,11 +6,14 @@ interface TickerProps {
 
 const Ticker: React.FC<TickerProps> = ({ messages }) => {
   return (
-    <div className="overflow-hidden bg-red-700 text-white font-bold h-12 flex items-center">
-      <div className="animate-marquee-left-to-right flex space-x-4">
+    <div className="overflow-hidden bg-red-700 text-white font-bold h-14 sm:h-16 md:h-18 lg:h-20 flex items-center">
+      <div className="animate-marquee-left-to-right flex space-x-6">
         {messages.map((message, index) => (
-          <span key={index} className="mx-4 uppercase">
-            {message} <span className="text-5xl">❄</span> {/* Adjust the text size */}
+          <span
+            key={index}
+            className="mx-4 uppercase text-xl sm:text-2xl md:text-3xl lg:text-4xl whitespace-nowrap"
+          >
+            {message} <span className="text-3xl sm:text-4xl">❄</span>
           </span>
         ))}
       </div>
