f69ce72381ab634aa19bde110e36b7eabf599815
danielschaaf <daniel.schaaf@smoothstack.com>
2025-01-08 20:28:01 -0800

    made adjustments to the buses information on school pages.

--- app/(routes)/aboutus/todays-leadership/page.tsx
+++ app/(routes)/aboutus/todays-leadership/page.tsx
@@ -13,19 +13,15 @@ const TodaysLeadership = () => {
   return (
     <div className="max-w-6xl mx-auto p-8">
       <h1 className="text-6xl font-bold text-blue-800 mb-6 text-center">
-        Todays Leadership Team
+        Today's Leadership Team
       </h1>
       <div className="grid grid-cols-3 gap-8">
-        {" "}
-        {/* Grid container */}
         {/* Column 1 */}
         <div className="space-y-4">
-          {" "}
           <Card className="flex flex-col h-full bg-gray-100">
             <CardHeader>
               <CardTitle className="text-center">DAN ALBERTINI</CardTitle>
               <CardDescription className="text-center text-black">
-                {" "}
                 STAFF SERVICES DIRECTOR & BOARD MEMBER
               </CardDescription>
             </CardHeader>
@@ -48,17 +44,14 @@ const TodaysLeadership = () => {
               </p>
             </CardFooter>
           </Card>
-          {/* Vertical spacing */}
         </div>
+
         {/* Column 2 */}
         <div className="space-y-4">
-          {" "}
-          {/* Vertical spacing */}
           <Card className="flex flex-col h-full bg-gray-100">
             <CardHeader>
               <CardTitle className="text-center">GORDY BALL</CardTitle>
               <CardDescription className="text-center text-black">
-                {" "}
                 TECHNICAL DIRECTOR & BOARD MEMBER
               </CardDescription>
             </CardHeader>
@@ -73,33 +66,30 @@ const TodaysLeadership = () => {
             </CardContent>
             <CardFooter className="text-center">
               <p>
-                {" "}
                 Gordy has over forty years experience teaching and leading ski
                 enthusiasts of all ages. He continues to serve the Professional
                 Ski Instructors of America (PSIA) as an examiner and has served
                 on the Board of Directors, and as a divisional clinic leader.
-                "Developing a community of skillful, joyful, student centered
+                "Developing a community of skillful, joyful, student-centered
                 instructors that lead students into successful mountain
                 experiences" continues to be his lifelong goal.
               </p>
             </CardFooter>
           </Card>
         </div>
+
         {/* Column 3 */}
         <div className="space-y-4">
-          {" "}
-          {/* Vertical spacing */}
           <Card className="flex flex-col h-full bg-gray-100">
             <CardHeader>
               <CardTitle className="text-center">TIM BERGLUND</CardTitle>
               <CardDescription className="text-center text-black">
-                {" "}
                 SUPERVISOR & BOARD MEMBER
               </CardDescription>
             </CardHeader>
             <CardContent className="flex justify-center">
               <Image
-                src="/Tim Ski photo 2.jpg" // Replace with the path to the third image
+                src="/Tim Ski photo 2.jpg"
                 alt="Tim Berglund"
                 width={300}
                 height={400}
@@ -108,112 +98,36 @@ const TodaysLeadership = () => {
             </CardContent>
             <CardFooter className="text-center">
               <p>
-                {" "}
                 Tim has been teaching with Mohan Ski School since 1989. Tim
                 began as an assistant while in junior high school, before being
                 promoted to instructor. While on staff for Mohan, Tim has served
                 as a full-time instructor for 5 yrs, resident desk supervisor,
-                ski and snowboard clinician and currently serves on the Mohan
+                ski and snowboard clinician, and currently serves on the Mohan
                 Board of Directors. Tim has lived by the motto taught by John
                 Mohan himself, "We are teaching people how to have fun, not how
-                to ski. People learn to ski, because they are having fun.
+                to ski. People learn to ski, because they are having fun."
               </p>
             </CardFooter>
           </Card>
         </div>
-        <br />
       </div>
-      <div className="grid gap-8">
-        {" "}
-        {/* Grid container */}
+
+      <div className="grid grid-cols-2 gap-8 mt-8">
         {/* Column 1 */}
-        <div className="space-y-4"> {/* Vertical spacing */}</div>
-        {/* Column 2 */}
-        <div className="space-y-4">
-          {" "}
-          {/* Vertical spacing */}
-          {/* <Card className="flex flex-col h-full bg-gray-100">
-            <CardHeader>
-              <CardTitle className="text-center">Nathaniel Gibbs</CardTitle>
-              <CardDescription className="text-center text-black-800">
-                General Manager
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="flex justify-center">
-              <Image
-                src="/NathanielGibbs.png" // Replace with the path to the third image
-                alt="Nathaniel Gibbs"
-                width={250}
-                height={200}
-                objectFit="cover"
-              />
-            </CardContent>
-            <CardFooter className="text-center">
-              <p>
-                {" "}
-                Nathaniel has been in the ski industry for 8+ years and has been
-                skiing for much longer. He enjoys being up on the mountain with
-                the fabulous Mohan team and the students that join us up on the
-                slopes. He is currently Mohan’s General Manager, and you will
-                see him in the community at events as well as up on the slopes
-                during the season. He also oversees the mountain operations
-                alongside the Technical Director and oversees the Mohan Office.
-                He loves to get to know all the Mohanian students and their
-           
-        </div>
-        {/* Column 3 */}
-        <div className="space-y-4"> {/* Vertical spacing */}</div>
-      </div>
-      <br />
-      <br />
-      <div className="grid grid-cols-2 gap-8">
-        {/* Column 1 */}
-        <div className="space-y-4">
-          {/* <Card className="flex flex-col h-full bg-gray-100">
-            <CardHeader>
-              <CardTitle className="text-center">Ruth Richardson</CardTitle>
-              <CardDescription className="text-center text-black-800">
-                OFFICE MANAGER
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="flex justify-center">
-              <Image
-                src="/RuthLodge.jpg" // Replace with the path to the third image
-                alt="Ruth Richardson"
-                width={200}
-                height={200}
-                objectFit="cover"
-              />
-            </CardContent>
-            <CardFooter className="text-center">
-              <p>
-                Ruth began volunteering in our office in 2017 while getting
-                reintroduced to skiing. She was a test student for all of our
-                instructors to finesse their skill with. She is well versed with
-                the Mohan teachings and brings over 25 years of practice
-                management to the Mohan Family. She is our point guard on and
-                off of the mountain for staff and student registration
-                protocols. She also coordinates all of our private lessons and
-                handles most of the emails and phone calls during the season.
-                Her personal goal in and out of the office is to flip an upside
-                down smile to the way it should be.
-              </p>
-            </CardFooter>
-          </Card> */}
-        </div>
+        <div className="space-y-4"></div>
 
         {/* Column 2 */}
         <div className="space-y-4">
-        <Card className="flex flex-col h-full bg-gray-100">
+          <Card className="flex flex-col h-full bg-gray-100">
             <CardHeader>
               <CardTitle className="text-center">Emily Sheehan</CardTitle>
               <CardDescription className="text-center text-black-800">
-              Program Coordinator Manager
+                Program Coordinator Manager
               </CardDescription>
             </CardHeader>
             <CardContent className="flex justify-center">
               <Image
-                src="/Emily.jpg" // Replace with the path to the third image
+                src="/Emily.jpg"
                 alt="Emily Sheehan"
                 width={200}
                 height={200}
@@ -222,7 +136,11 @@ const TodaysLeadership = () => {
             </CardContent>
             <CardFooter className="text-center">
               <p>
-              An avid skier and snow enthusiast, Emily loves being up on the mountain with the team at Mohan. On top of over 25+ years of ski experience (she’s been skiing as long as she could walk), she is a PSIA Level I Instructor with over 2+ years in the skiing industry. While not working in the office or instructing students, she’s out with her gimbal on the slopes making movies for Mohan. She works in the office to make sure all your questions are answered quickly and efficiently, so that you’re all set to buckle up your boots and get out there! She loves working with new students and building community within Mohan. When she’s not skiing (or answering skiing related emails,) Emily works as a narrative/commercial film director/producer.  
+                An avid skier and snow enthusiast, Emily loves being up on the
+                mountain with the team at Mohan. On top of over 25+ years of ski
+                experience (she’s been skiing as long as she could walk), she is
+                a PSIA Level I Instructor with over 2+ years in the skiing
+                industry.
               </p>
             </CardFooter>
           </Card>
